0b8536db687c8f522b72b41f2daa9701e1424412
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-12-06 15:43:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	return date1 - date2;
 };
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+//alert(dateDiffInDays(dateOne, dateTwo));
